58d8f22fd5e5b42c665003c2a9c55a5c26287ec6
Swayam Terode <swayam.programmer@gmail.com>
2024-02-21 12:07:46 +0530

    Refactor tech stack page component

--- app/tech-stack/page.tsx
+++ app/tech-stack/page.tsx
@@ -1,14 +1,13 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
-import { cards } from "../constants/data";
 type Props = {};
 
 const page = (props: Props) => {
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    fetch(process.env.NEXT_PUBLIC_API_URL+"/api/tech-stack")
+    fetch(process.env.NEXT_PUBLIC_API_URL + "/api/tech-stack")
       .then((response) => response.json())
       .then((data) => setData(data))
       .catch((error) => console.error("Error:", error));
@@ -33,39 +32,43 @@ const page = (props: Props) => {
           Dev & Design
         </h1>
         <div className="grid md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 w-full md:w-[70%] mx-auto gap-4 p-6 md:p-0">
-          {data && (data as Array<any>).map((card, index: number) => (
-            <div
-              key={index}
-              className="flex flex-col relative bg-[#F3F3F3] dark:bg-[#181818] mx-auto w-full h-[86px] md:h-[300px] rounded-3xl"
-            >
-              <div className="flex justify-center items-center">
-                <Image
-                  src={card.src}
-                  alt={card.alt}
-                  width={card.width}
-                  height={card.height}
-                  className={card.className}
-                />
-                <div className="absolute bottom-3 md:bottom-5 flex justify-start mx-auto w-full px-5 items-center">
+          {data &&
+            (data as Array<any>).map((card, index: number) => (
+              <div
+                key={index}
+                className="flex flex-col relative bg-[#F3F3F3] dark:bg-[#181818] mx-auto w-full h-[86px] md:h-[300px] rounded-3xl"
+              >
+                <div className="flex justify-center items-center">
+                  {/* For md devices to 2xl */}
                   <Image
                     src={card.src}
                     alt={card.alt}
-                    width={60}
-                    height={200}
-                    className="md:hidden"
+                    width={card.width}
+                    height={card.height}
+                    quality={100}
+                    className="w-[45px] mt-24 md:w-[70px] 2xl:mt-20 hidden md:block"
                   />
-                  <div className="flex w-full justify-between items-center pl-4">
-                    <span className="flex md:text-xl md:font-semibold">
-                      {card.title}
-                    </span>
-                    <span className="flex border rounded-3xl px-3 py-1 border-white bg-white dark:bg-[#181818] dark:border-[#3C3C3C] text-[#808080] text-xs">
-                      {card.category}
-                    </span>
+                  {/* For Mobile */}
+                  <div className="absolute bottom-4 md:bottom-5 flex justify-center mx-auto w-full px-4 items-center">
+                    <Image
+                      src={card.src}
+                      alt={card.alt}
+                      width={50}
+                      height={100}
+                      className="md:hidden"
+                    />
+                    <div className="flex w-full justify-between items-center pl-4">
+                      <span className="flex text-sm md:text-xl md:font-semibold">
+                        {card.title}
+                      </span>
+                      <span className="flex border rounded-3xl px-3 py-1 border-white bg-white dark:bg-[#181818] dark:border-[#3C3C3C] text-[#808080] text-xs">
+                        {card.category}
+                      </span>
+                    </div>
                   </div>
                 </div>
               </div>
-        </div>
-          ))}
+            ))}
         </div>
       </div>
     </>
