63079bcd49ac2c06e1bb89af9c270a8b5c611af6
Swayam Terode <swayamterodex@gmail.com>
2024-05-06 01:20:31 +0530

    feat(toast): add react-toastify

---
+++ utils/Toastify.ts
@@ -0,0 +1,29 @@
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const Toastify = {
+  success: (message: string) => {
+    toast.success(message, {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  },
+  error: (message: string) => {
+    toast.error(message, {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  },
+};
+
+export default Toastify;
