fe8b69d6708354be4b8faf1d10a33614deb16718
Swayam Terode <swayamterodex@gmail.com>
2024-05-06 01:30:58 +0530

    chore(project): add project icon

--- constants/project.ts
+++ constants/project.ts
@@ -4,7 +4,7 @@ export const projects = [
     title: "Parkbuddy",
     description:
       "Subbi is a side project that I’ve built to help me keep track of how much I spend on subscriptions and also to prevent the “accidental” bill after a 14-day trail ends. It helps you keep track of bills like Netflix, Spotify, Xbox Game Pass, Bus Card, Bank Cards, and many more.",
-    logo: "/Logo.svg",
+    logo: "/parkbuddy.svg",
     link: "https://parkbuddy.vercel.app/",
     github: "https://github.com/swayamterode/Parkbuddy",
     image: "/ParkbuddyMockup.png",
@@ -14,7 +14,7 @@ export const projects = [
     title: "Watchflix",
     description:
       "Stream seamlessly with user-friendly interface powered by Tailwind CSS and React. Discover movies effortlessly using mood-based searches through the Open AI API. Enjoy a global experience with multilingual support. Secure logins and sign-ups through Firebase, and comprehensive movie data fetched via the TMDB API.",
-    logo: "/Logo.svg",
+    logo: "/watchflix.svg",
     link: "https://watchflix-gpt-ai.web.app/",
     github: "https://github.com/swayamterode/WatchflixGPT",
     image: "/ParkbuddyMockup.png",
