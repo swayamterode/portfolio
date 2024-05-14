d6bb4b44911f348fc8da7bcbab504cf04efb7146
Swayam Terode <swayamterodex@gmail.com>
2024-05-15 01:17:29 +0530

    fix(typo): import the correct module

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import SocialLinks from "@/utils/socialLinks";
+import SocialLinks from "@/utils/SocialLinks";
 interface Props {}
 
 const Footer = (props: Props) => {
