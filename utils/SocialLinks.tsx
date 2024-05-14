6e2e7e80128d8e6a0404b01680a073adda315097
Swayam Terode <swayamterodex@gmail.com>
2024-05-15 01:14:46 +0530

    :fix(socialLinks): Implemented Map and check for isFooter component

--- utils/SocialLinks.tsx
+++ utils/SocialLinks.tsx
@@ -1,15 +1,36 @@
 import React from "react";
-interface Props {
-  url: string;
+import {
+  GITHUB_URL,
+  LINKEDIN_URL,
+  TWITTER_URL,
+  MAIL_URL,
+} from "@/constants/data";
+
+type Props = {
   name: string;
-}
-const SocialLinks = ({ url, name }: Props) => {
+  isFooter?: boolean;
+};
+
+let links = new Map<string, string>([
+  ["GitHub", GITHUB_URL],
+  ["LinkedIn", LINKEDIN_URL],
+  ["Twitter", TWITTER_URL],
+  ["Mail", MAIL_URL],
+]);
+
+const SocialLinks = ({ name, isFooter }: Props) => {
+  const link = links.get(name);
+
   return (
     <a
-      href={url}
+      href={link}
       target="_blank"
-      rel="noreferrer"
-      className="text-black font-semibold dark:text-white underline cursor-pointer"
+      rel="noopener noreferrer"
+      className={
+        isFooter
+          ? `text-xs hover:text-gray-700 dark:hover:text-white`
+          : `text-black font-semibold dark:text-white underline cursor-pointer`
+      }
     >
       {name}
     </a>
