104b047a82cc47b99d79d88cb272e77a72a38231
Swayam Terode <swayamterodex@gmail.com>
2024-05-15 17:36:43 +0530

    chore(db):add timestamps

--- lib/modals/About_me.ts
+++ lib/modals/About_me.ts
@@ -1,13 +1,16 @@
 import { Schema, model, models } from "mongoose";
 
-const aboutMeSchema = new Schema({
-  // Who I am section
-  heading1: { type: String, required: true },
-  description1: { type: String, required: true },
-  // What I do section
-  heading2: { type: String, required: true },
-  description2: { type: String, required: true },
-});
+const aboutMeSchema = new Schema(
+  {
+    // Who I am section
+    heading1: { type: String, required: true },
+    description1: { type: String, required: true },
+    // What I do section
+    heading2: { type: String, required: true },
+    description2: { type: String, required: true },
+  },
+  { timestamps: true },
+);
 
 const AboutSection =
   models.AboutSection || model("AboutSection", aboutMeSchema);
