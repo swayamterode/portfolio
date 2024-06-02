82d18d6cc7610c55451b1676040f64d4d9b14d84
Swayam Terode <swayamterodex@gmail.com>
2024-06-02 14:40:34 +0530

    build: snipper

---
+++ components/snipper/snipper.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./snipper.css";
+type Props = {};
+
+const snipper = (props: Props) => {
+  return <span className="loader"></span>;
+};
+
+export default snipper;
